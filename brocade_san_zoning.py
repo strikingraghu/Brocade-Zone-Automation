d0c14d9e4426719d160782c48934439bdf1ba5d4
strikingraghu <strikingraghu@gmail.com>
2021-06-17 18:04:17 +0530

    "Added documentation to the script functions"

---
+++ brocade_san_zoning.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python
+# The term 'Broadcom' refers to Broadcom Inc. and/or its subsidiaries.
+
+DOCUMENTATION = '''
+Python code developed for automating 'Brocade SAN Zoning' tasks performed by Network/Storage team.
+Functions:
+    a) Logging into Brocade switch
+    b) Taking backup of existing switch configuration
+    c) Taking backup of existing zone configuration
+    d) Creating alias
+    e) Creating zone
+    f) Adding new zone configuration
+    g) Saving new zone configuration
+    h) Taking backup of switch after configuration changes
+    i) Taking backup of zone configuration after changes
+    j) Enabling port
+    k) Disabling Port
+    l) Logout from the Brocade switch
+'''
