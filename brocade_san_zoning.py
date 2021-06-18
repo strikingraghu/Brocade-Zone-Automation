b3de82d2ed2dc3072d8ab0ab30d9d6e54daf78db
strikingraghu <strikingraghu@gmail.com>
2021-06-18 13:59:23 +0530

    "Added login function"

--- brocade_san_zoning.py
+++ brocade_san_zoning.py
@@ -1,6 +1,9 @@
 #!/usr/bin/python
 # The term 'Broadcom' refers to Broadcom Inc. and/or its subsidiaries.
 
+from requests.auth import HTTPBasicAuth
+import requests
+
 DOCUMENTATION = '''
 Python code developed for automating 'Brocade SAN Zoning' tasks performed by Network/Storage team.
 Functions:
@@ -17,3 +20,26 @@ Functions:
     k) Disabling Port
     l) Logout from the Brocade switch
 '''
+
+
+def brocade_san_switch_login(ipaddress, username, password):
+    """
+        :param ipaddress: Provide switch IP address
+        :param username: Provide switch username (REST API user)
+        :param password: Provide switch password (REST API user password)
+        :return: Bearer token for subsequent calls
+    """
+    try:
+        login_endpoint = 'http://' + ipaddress + '/rest/login'
+        login = requests.request("POST", login_endpoint, auth=HTTPBasicAuth(username, password))
+        print("Brocade login status code: ", login.status_code)
+        if login.status_code == 200:
+            login_bearer_token = login.headers.get('Authorization')
+            print("Login API key retrieved: ", login_bearer_token)
+            print("Brocade switch login successful - ", ipaddress)
+    except Exception as e:
+        print("Brocade switch login not successful - ", ipaddress)
+        print(e)
+
+
+brocade_san_switch_login('172.24.2.113', 'rathnar', 'rathnar56')
