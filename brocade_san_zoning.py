4ac396975cfb9cdcf47421e61a319a71a3354628
strikingraghu <strikingraghu@gmail.com>
2021-07-03 22:11:16 +0530

    Added alias creation function

--- brocade_san_zoning.py
+++ brocade_san_zoning.py
@@ -68,5 +68,29 @@ def brocade_san_switch_config_backup(custom_token, ipaddress):
         print(e)
 
 
+def brocade_san_switch_alias_creation(custom_token, ipaddress, alias_name, alias_entry_name):
+    """
+    :param custom_token: Provide custom token fetched in previous call
+    :param ipaddress: Provide switch IP address
+    :param alias_name: Provide alias name from the payload
+    :param alias_entry_name: Provide WWN value from the payload for alias
+    :return: alias creation status
+    """
+    global switch_config_backup_json
+    call_config_endpoint = ""
+    try:
+        switch_config_endpoint = 'http://' + ipaddress + '/rest/running/brocade-zone/effective-configuration'
+        call_headers = {'Authorization': custom_token, 'Accept': 'application/yang-data+json',
+                        'Content-Type': 'application/yang-data+json'}
+        call_config_endpoint = requests.get(url=switch_config_endpoint, headers=call_headers)
+        print("Switch zone data: ", call_config_endpoint)
+        switch_config_backup_json = json.loads(call_config_endpoint.content)
+        if call_config_endpoint.status_code == 200:
+            print("Switch zone information backed up - ", switch_config_backup_json)
+    except Exception as e:
+        print("Brocade switch login token or endpoint issues - ", call_config_endpoint.status_code)
+        print(e)
+
+
 brocade_san_switch_login('10.60.22.214', 'admin', 'ctcemc123')
 brocade_san_switch_config_backup(custom_basic_key, '10.60.22.214')
