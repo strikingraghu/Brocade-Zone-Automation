a7532c77341f7af9e26113b2816e32868a9384bb
strikingraghu <strikingraghu@gmail.com>
2021-07-03 20:46:25 +0530

    Brocade zone automation functions

--- brocade_san_zoning.py
+++ brocade_san_zoning.py
@@ -9,17 +9,16 @@ DOCUMENTATION = '''
 Python code developed for automating 'Brocade SAN Zoning' tasks performed by Network/Storage team.
 Functions:
     a) Logging into Brocade switch
-    b) Taking backup of existing switch configuration
-    c) Taking backup of existing zone configuration
-    d) Creating alias
-    e) Creating zone
-    f) Adding new zone configuration
-    g) Saving new zone configuration
-    h) Taking backup of switch after configuration changes
-    i) Taking backup of zone configuration after changes
-    j) Enabling port
-    k) Disabling Port
-    l) Logout from the Brocade switch
+    b) Taking backup of existing zone configuration
+    c) Creating alias
+    d) Creating zone
+    e) Adding new zone configuration
+    f) Saving new zone configuration
+    g) Taking backup of switch after configuration changes
+    h) Taking backup of zone configuration after changes
+    i) Enabling port
+    j) Disabling Port
+    k) Logout from the Brocade switch
 '''
 
 custom_basic_key = ""
