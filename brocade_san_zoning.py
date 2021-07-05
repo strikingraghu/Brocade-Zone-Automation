6fb95bc3503d163fc4021de55cfe97edfd570c46
strikingraghu <strikingraghu@gmail.com>
2021-07-05 13:17:28 +0530

    Added alias creation function

--- brocade_san_zoning.py
+++ brocade_san_zoning.py
@@ -21,7 +21,8 @@ Functions:
     k) Logout from the Brocade switch
 '''
 
-custom_basic_key = ""
+# Generic Global Variables
+custom_api_key = ""
 switch_config_backup_json = ""
 
 
@@ -32,65 +33,69 @@ def brocade_san_switch_login(ipaddress, username, password):
         :param password: Provide switch password (REST API user password)
         :return: Bearer token for subsequent calls
     """
-    global custom_basic_key
+    global custom_api_key
     try:
-        login_endpoint = 'http://' + ipaddress + '/rest/login'
-        login = requests.request("POST", login_endpoint, auth=HTTPBasicAuth(username, password))
+        login_url = 'http://' + ipaddress + '/rest/login'
+        login = requests.request("POST", login_url, auth=HTTPBasicAuth(username, password))
         print("Brocade login status code: ", login.status_code)
         if login.status_code == 200:
-            custom_basic_key = login.headers.get('Authorization')
-            print("Login API key retrieved: ", custom_basic_key)
+            custom_api_key = login.headers.get('Authorization')
+            print("Login API key retrieved: ", custom_api_key)
             print("Brocade switch login successful - ", ipaddress)
     except Exception as e:
         print("Brocade switch login not successful - ", ipaddress)
         print(e)
 
 
-def brocade_san_switch_config_backup(custom_token, ipaddress):
+def brocade_san_switch_config_backup(ipaddress):
     """
-    :param custom_token: Provide custom token fetched in previous call
     :param ipaddress: Provide switch IP address
     :return: Switch configuration data (Backup purpose)
     """
     global switch_config_backup_json
-    call_config_endpoint = ""
+    call_config_backup = ""
     try:
-        switch_config_endpoint = 'http://' + ipaddress + '/rest/running/brocade-zone/effective-configuration'
-        call_headers = {'Authorization': custom_token, 'Accept': 'application/yang-data+json',
-                        'Content-Type': 'application/yang-data+json'}
-        call_config_endpoint = requests.get(url=switch_config_endpoint, headers=call_headers)
-        print("Switch zone data: ", call_config_endpoint)
-        switch_config_backup_json = json.loads(call_config_endpoint.content)
-        if call_config_endpoint.status_code == 200:
+        switch_config_url = 'http://' + ipaddress + '/rest/running/brocade-zone/effective-configuration'
+        switch_config_backup_call_headers = {'Authorization': custom_api_key, 'Accept': 'application/yang-data+json',
+                                             'Content-Type': 'application/yang-data+json'}
+        call_config_backup = requests.get(url=switch_config_url, headers=switch_config_backup_call_headers)
+        print("Switch zone data: ", call_config_backup)
+        switch_config_backup_json = json.loads(call_config_backup.content)
+        if call_config_backup.status_code == 200:
             print("Switch zone information backed up - ", switch_config_backup_json)
     except Exception as e:
-        print("Brocade switch login token or endpoint issues - ", call_config_endpoint.status_code)
+        print("Brocade switch login token or endpoint issues - ", call_config_backup.status_code)
         print(e)
 
 
-def brocade_san_switch_alias_creation(custom_token, ipaddress, alias_name, alias_entry_name):
+def brocade_san_switch_alias_creation(alias_name, alias_entry_name, ipaddress):
     """
-    :param custom_token: Provide custom token fetched in previous call
-    :param ipaddress: Provide switch IP address
     :param alias_name: Provide alias name from the payload
     :param alias_entry_name: Provide WWN value from the payload for alias
+    :param ipaddress: Provide switch IP address
     :return: alias creation status
     """
-    global switch_config_backup_json
-    call_config_endpoint = ""
+    switch_create_alias = ""
+    switch_alias_creation_call_headers = {'Authorization': custom_api_key, 'Accept': 'application/yang-data+json',
+                                          'Content-Type': 'application/yang-data+json'}
+    call_data = '{"alias":{"alias-name": "' + alias_name + '", "member-entry": {"alias-entry-name": "' + \
+                alias_entry_name + '"}}}'
+    json_transformation = json.loads(call_data)
+    print('Call body being used in this function: ', json_transformation)
+    print()
     try:
-        switch_config_endpoint = 'http://' + ipaddress + '/rest/running/brocade-zone/effective-configuration'
-        call_headers = {'Authorization': custom_token, 'Accept': 'application/yang-data+json',
-                        'Content-Type': 'application/yang-data+json'}
-        call_config_endpoint = requests.get(url=switch_config_endpoint, headers=call_headers)
-        print("Switch zone data: ", call_config_endpoint)
-        switch_config_backup_json = json.loads(call_config_endpoint.content)
-        if call_config_endpoint.status_code == 200:
-            print("Switch zone information backed up - ", switch_config_backup_json)
+        switch_create_alias_url = 'http://' + ipaddress + '/rest/running/brocade-zone/defined-configuration/alias'
+        switch_create_alias = requests.post(url=switch_create_alias_url, headers=switch_alias_creation_call_headers,
+                                            data=call_data)
+        print("Alias creation status: ", switch_create_alias)
+        switch_alias_create_json = json.loads(switch_create_alias.content)
+        if switch_create_alias.status_code == 200:
+            print("Alias creation is successful - ", switch_alias_create_json)
     except Exception as e:
-        print("Brocade switch login token or endpoint issues - ", call_config_endpoint.status_code)
+        print("Brocade switch login token or endpoint issues - ", switch_create_alias.status_code)
         print(e)
 
 
 brocade_san_switch_login('10.60.22.214', 'admin', 'ctcemc123')
-brocade_san_switch_config_backup(custom_basic_key, '10.60.22.214')
+brocade_san_switch_config_backup('10.60.22.214')
+brocade_san_switch_alias_creation('Axel_spa_A1Port3_Test', '50:06:01:63:08:60:1d:e8', '10.60.22.214')
