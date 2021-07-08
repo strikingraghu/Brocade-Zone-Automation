033e90123a7c10d618eb9a90843b58109ce66907
strikingraghu <strikingraghu@gmail.com>
2021-07-08 19:49:19 +0530

    Added few more modifications to the code

--- brocade_san_zoning.py
+++ brocade_san_zoning.py
@@ -4,6 +4,7 @@
 from requests.auth import HTTPBasicAuth
 import requests
 import json
+import time
 
 DOCUMENTATION = '''
 Python code developed for automating 'Brocade SAN Zoning' tasks performed by Network/Storage team.
@@ -22,7 +23,7 @@ Functions:
 '''
 
 # Generic Global Variables
-custom_api_key = "Custom_Basic YWRtaW46eHh4OmQ4NDE1ZGI5YTJiYjVhMjRiMTFkYWYxODFmOTIxYTk1NGI0NmM4NTk1OWFlZmQyYWQ2N2U1MjVlNTUwODUzNGE="
+custom_api_key = ""
 switch_config_backup_json = ""
 
 
@@ -38,6 +39,7 @@ def brocade_san_switch_login(ipaddress, username, password):
         login_url = 'http://' + ipaddress + '/rest/login'
         login = requests.request("POST", login_url, auth=HTTPBasicAuth(username, password))
         print("Brocade login status code: ", login.status_code)
+        time.sleep(10)
         if login.status_code == 200:
             custom_api_key = login.headers.get('Authorization')
             print("Login API key retrieved: ", custom_api_key)
@@ -57,12 +59,12 @@ def brocade_san_switch_config_backup(ipaddress):
     call_config_backup = ""
     try:
         switch_config_url = 'http://' + ipaddress + '/rest/running/brocade-zone/effective-configuration'
-        switch_config_backup_call_headers = {'Authorization': custom_api_key, 'Accept': 'application/yang-data+json',
-                                             'Content-Type': 'application/yang-data+json'}
+        switch_config_backup_call_headers = {'Authorization': custom_api_key, 'Accept': 'application/yang-data+json', 'Content-Type': 'application/yang-data+json'}
         call_config_backup = requests.get(url=switch_config_url, headers=switch_config_backup_call_headers)
         print("Switch zone data: ", call_config_backup)
-        switch_config_backup_json = json.loads(call_config_backup.content)  # Type conversion to dict is required
+        switch_config_backup_json = json.dumps(call_config_backup.content, indent=2)  # Type conversion to dict is required
         print("Configuration data of the SAN switch before automation: ", switch_config_backup_json)
+        time.sleep(10)
         if call_config_backup.status_code == 200:
             print("Switch zone information backed up - ", switch_config_backup_json)
     except Exception as e:
@@ -79,8 +81,7 @@ def brocade_san_switch_alias_creation(alias_name, alias_entry_name, ipaddress):
     :return: alias creation status
     """
     switch_create_alias = ""
-    switch_alias_creation_call_headers = {'Authorization': custom_api_key, 'Accept': 'application/yang-data+json',
-                                          'Content-Type': 'application/yang-data+json'}
+    switch_alias_creation_call_headers = {'Authorization': custom_api_key, 'Accept': 'application/yang-data+json', 'Content-Type': 'application/yang-data+json'}
     call_data = {"alias": {"alias-name": alias_name, "member-entry": {"alias-entry-name": alias_entry_name}}}
     json_transformation = json.dumps(call_data, indent=2)
     print('Call body type being used in this function: ', type(json_transformation), json_transformation)
@@ -90,6 +91,7 @@ def brocade_san_switch_alias_creation(alias_name, alias_entry_name, ipaddress):
                                             data=json_transformation)
         print("Alias creation status: ", switch_create_alias.status_code)
         switch_alias_create_json = json.loads(switch_create_alias.content)
+        time.sleep(10)
         if switch_create_alias.status_code == 201:
             print("Alias creation is successful - ", switch_alias_create_json)
     except Exception as e:
@@ -107,19 +109,16 @@ def brocade_san_switch_zone_creation(zone_name, zone_member_entry_name_1, zone_m
     :return: zone creation status
     """
     switch_create_zone_element = ""
-    switch_zone_creation_api_headers = {'Authorization': custom_api_key, 'Accept': 'application/yang-data+json',
-                                        'Content-Type': 'application/yang-data+json'}
-    call_data = {"zone": {"zone-name": zone_name, "zone-type": 0, "member-entry": {"entry-name":
-                                                                                       [zone_member_entry_name_1,
-                                                                                        zone_member_entry_name_2]}}}
+    switch_zone_creation_api_headers = {'Authorization': custom_api_key, 'Accept': 'application/yang-data+json', 'Content-Type': 'application/yang-data+json'}
+    call_data = {"zone": {"zone-name": zone_name, "zone-type": 0, "member-entry": {"entry-name": [zone_member_entry_name_1, zone_member_entry_name_2]}}}
     json_transformation = json.dumps(call_data, indent=2)
     print('Call body being used in this function: ', type(json_transformation), json_transformation)
     try:
         switch_create_zone_url = 'http://' + ipaddress + '/rest/running/brocade-zone/defined-configuration/zone'
-        switch_create_zone_element = requests.post(url=switch_create_zone_url, headers=switch_zone_creation_api_headers,
-                                                   data=json_transformation)
+        switch_create_zone_element = requests.post(url=switch_create_zone_url, headers=switch_zone_creation_api_headers, data=json_transformation)
         print("Zone creation status: ", switch_create_zone_element)
-        switch_create_zone_json = json.loads(switch_create_zone_element.content)
+        switch_create_zone_json = json.dumps(switch_create_zone_element.content, indent=2)
+        time.sleep(10)
         if switch_create_zone_element.status_code == 201:
             print("Zone creation is successful - ", switch_create_zone_json)
     except Exception as e:
@@ -135,24 +134,16 @@ def brocade_san_switch_zone_config_update(config_name, ipaddress):
     :return: zone save status
     """
     switch_save_zone_element = ""
-    switch_zone_config_update_api_headers = {'Authorization': custom_api_key, 'Accept': 'application/yang-data+json',
-                                             'Content-Type': 'application/yang-data+json'}
-    call_data = {"cfg": {"cfg-name": config_name, "member-zone": {"zone-name": ["Axel_Dellpr740A",
-                                                                                "Axel_Dellpr740C",
-                                                                                "Axel_Rodge_SPA",
-                                                                                "Axel_Rodge_SPB",
-                                                                                "Rodge-Dellpr730H",
-                                                                                "Rodge_Dellpr730E",
-                                                                                "dellpr730B_Marge"]}}}
-    json_transformation = json.dumps(call_data, indent=3)
+    switch_zone_config_update_api_headers = {'Authorization': custom_api_key, 'Accept': 'application/yang-data+json', 'Content-Type': 'application/yang-data+json'}
+    call_data = {"cfg": {"cfg-name": config_name, "member-zone": {"zone-name": ["Axel_Dellpr740A", "Axel_Dellpr740C", "Axel_Rodge_SPA", "Axel_Rodge_SPA_Test", "Axel_Rodge_SPB", "Rodge-Dellpr730H", "Rodge_Dellpr730E", "dellpr730B_Marge"]}}}
+    json_transformation = json.dumps(call_data, indent=2)
     print('Call body being used in this function: ', json_transformation)
     try:
         switch_save_zone_url = 'http://' + ipaddress + '/rest/running/brocade-zone/defined-configuration/cfg'
-        switch_save_zone_element = requests.patch(url=switch_save_zone_url,
-                                                  headers=switch_zone_config_update_api_headers,
-                                                  data=json_transformation)
+        switch_save_zone_element = requests.patch(url=switch_save_zone_url, headers=switch_zone_config_update_api_headers, data=json_transformation)
         print("Zone configuration update status: ", switch_save_zone_element)
-        switch_save_zone_json = json.loads(switch_save_zone_element.content)
+        switch_save_zone_json = json.dumps(switch_save_zone_element.content, indent=2)
+        time.sleep(10)
         if switch_save_zone_element.status_code == 204:
             print("Zone configuration update is successful - ", switch_save_zone_json)
     except Exception as e:
@@ -168,20 +159,17 @@ def brocade_san_switch_save_checksum(checksum_value, ipaddress):
     :return: Saving configuration to DB status code
     """
     switch_zonedb_save_element = ""
-    switch_zone_config_save_zonedb_api_headers = {'Authorization': custom_api_key,
-                                                  'Accept': 'application/yang-data+json',
-                                                  'Content-Type': 'application/yang-data+json'}
+    switch_zone_config_save_zonedb_api_headers = {'Authorization': custom_api_key, 'Accept': 'application/yang-data+json', 'Content-Type': 'application/yang-data+json'}
     call_data = {"checksum": checksum_value}
     json_transformation = json.dumps(call_data, indent=2)
     print('Call body being used in this function: ', json_transformation)
     try:
-        switch_zonedb_save_url = 'http://' + ipaddress + '/rest/running/brocade-zone/effective-configuration' \
-                                                         '/cfg-action/1'
-        switch_zonedb_save_element = requests.patch(url=switch_zonedb_save_url,
-                                                    headers=switch_zone_config_save_zonedb_api_headers,
-                                                    data=json_transformation)
-        print("Zone configuration update status: ", switch_zonedb_save_element)
-        switch_zonedb_save_zone_json = json.loads(switch_zonedb_save_element.content)
+        switch_zonedb_save_url = 'http://' + ipaddress + '/rest/running/brocade-zone/effective-configuration/cfg-action/1'
+        print(switch_zonedb_save_url)
+        switch_zonedb_save_element = requests.patch(url=switch_zonedb_save_url, headers=switch_zone_config_save_zonedb_api_headers, data=json_transformation)
+        print("Zone configuration update status: ", switch_zonedb_save_element.content)
+        switch_zonedb_save_zone_json = json.dumps(switch_zonedb_save_element.content, indent=2)
+        time.sleep(10)
         if switch_zonedb_save_element.status_code == 204:
             print("Zone configuration is saved to device successful - ", switch_zonedb_save_zone_json)
     except Exception as e:
@@ -196,16 +184,13 @@ def brocade_san_switch_get_new_checksum(ipaddress):
     :return: Returns a new checksum value
     """
     switch_zone_get_new_checksum_element = ""
-    switch_zone_get_new_checksum_api_headers = {'Authorization': custom_api_key,
-                                                'Accept': 'application/yang-data+json',
-                                                'Content-Type': 'application/yang-data+json'}
+    switch_zone_get_new_checksum_api_headers = {'Authorization': custom_api_key, 'Accept': 'application/yang-data+json', 'Content-Type': 'application/yang-data+json'}
     try:
-        switch_zone_get_new_checksum_url = 'http://' + ipaddress + '/rest/running/brocade-zone/' \
-                                                                   'effective-configuration/checksum'
-        switch_zone_get_new_checksum_element = requests.get(url=switch_zone_get_new_checksum_url,
-                                                            headers=switch_zone_get_new_checksum_api_headers)
+        switch_zone_get_new_checksum_url = 'http://' + ipaddress + '/rest/running/brocade-zone/effective-configuration/checksum'
+        switch_zone_get_new_checksum_element = requests.get(url=switch_zone_get_new_checksum_url, headers=switch_zone_get_new_checksum_api_headers)
         print("Switch new checksum value: ", switch_zone_get_new_checksum_element)
-        switch_zone_get_new_checksum_json = json.loads(switch_zone_get_new_checksum_element.content)
+        switch_zone_get_new_checksum_json = json.dumps(switch_zone_get_new_checksum_element.content, indent=2)
+        time.sleep(10)
         if switch_zone_get_new_checksum_element.status_code == 200:
             print("Getting new checksum value successful - ", switch_zone_get_new_checksum_json)
     except Exception as e:
@@ -222,20 +207,16 @@ def brocade_san_switch_enable_new_config(checksum_value, config_name, ipaddress)
     :return: Enabling new configuration on the SAN switch
     """
     switch_zone_enable_new_config_element = ""
-    switch_zone_enable_new_config_api_headers = {'Authorization': custom_api_key,
-                                                 'Accept': 'application/yang-data+json',
-                                                 'Content-Type': 'application/yang-data+json'}
+    switch_zone_enable_new_config_api_headers = {'Authorization': custom_api_key, 'Accept': 'application/yang-data+json', 'Content-Type': 'application/yang-data+json'}
     call_data = {"checksum": checksum_value}
     json_transformation = json.dumps(call_data, indent=2)
     print('Call body being used in this function: ', json_transformation)
     try:
-        switch_zone_enable_new_config_url = 'http://' + ipaddress + '/rest/running/brocade-zone/' \
-                                                                    'effective-configuration/cfg-name/' + config_name
-        switch_zone_enable_new_config_element = requests.patch(url=switch_zone_enable_new_config_url,
-                                                               headers=switch_zone_enable_new_config_api_headers,
-                                                               data=json_transformation)
+        switch_zone_enable_new_config_url = 'http://' + ipaddress + '/rest/running/brocade-zone/effective-configuration/cfg-name/' + config_name
+        switch_zone_enable_new_config_element = requests.patch(url=switch_zone_enable_new_config_url, headers=switch_zone_enable_new_config_api_headers, data=json_transformation)
         print("Zone configuration enablement status via new checksum: ", switch_zone_enable_new_config_element)
-        switch_zone_enable_new_config_json = json.loads(switch_zone_enable_new_config_element.content)
+        switch_zone_enable_new_config_json = json.dumps(switch_zone_enable_new_config_element.content, indent=2)
+        time.sleep(10)
         if switch_zone_enable_new_config_element.status_code == 204:
             print("Zone configuration is saved to device successful - ", switch_zone_enable_new_config_json)
     except Exception as e:
@@ -245,13 +226,13 @@ def brocade_san_switch_enable_new_config(checksum_value, config_name, ipaddress)
 
 
 brocade_san_switch_login('10.60.22.214', 'admin', 'ctcemc123')
-# brocade_san_switch_config_backup('10.60.22.214')
-# brocade_san_switch_alias_creation('Axel_spa_A1Port3_Test', '50:06:01:63:08:60:1d:e8', '10.60.22.214')
-# brocade_san_switch_alias_creation('Rodge_spa_A4Port3_Test', '50:06:01:63:08:64:0f:45', '10.60.22.214')
-# brocade_san_switch_zone_creation('Axel_Rodge_SPA', 'Axel_spa_A1Port3', 'Rodge_spa_A4Port3', '10.60.22.214')
-# brocade_san_switch_zone_creation('Axel_Rodge_SPA_Test', 'Axel_spa_A1Port3_Test', 'Rodge_spa_A4Port3_Test',
-#                                 '10.60.22.214')
-# brocade_san_switch_zone_config_update('b238638', '10.60.22.214')
-# brocade_san_switch_save_checksum('c9712b1df97e2cb9d2e692796d6fd415', '10.60.22.214')
+brocade_san_switch_config_backup('10.60.22.214')
+brocade_san_switch_alias_creation('Axel_spa_A1Port3_Test', '50:06:01:63:08:60:1d:e8', '10.60.22.214')
+brocade_san_switch_alias_creation('Rodge_spa_A4Port3_Test', '50:06:01:63:08:64:0f:45', '10.60.22.214')
+brocade_san_switch_zone_creation('Axel_Rodge_SPA', 'Axel_spa_A1Port3', 'Rodge_spa_A4Port3', '10.60.22.214')
+brocade_san_switch_zone_creation('Axel_Rodge_SPA_Test', 'Axel_spa_A1Port3_Test', 'Rodge_spa_A4Port3_Test',
+                                 '10.60.22.214')
+brocade_san_switch_zone_config_update('b238638', '10.60.22.214')
+brocade_san_switch_save_checksum('e5e1e7a4919a0fd961159245740aebfd', '10.60.22.214')
 # brocade_san_switch_get_new_checksum('10.60.22.214')
-# brocade_san_switch_enable_new_config('bb5fc695fd1b28eb8856de776a244c35', 'b238638', '10.60.22.214')
+# brocade_san_switch_enable_new_config('e5e1e7a4919a0fd961159245740aebfd', 'b238638', '10.60.22.214')
