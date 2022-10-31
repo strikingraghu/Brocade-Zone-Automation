8ef87397eb7e4c36d58f0355aff343e28483516a
strikingraghu <strikingraghu@gmail.com>
2022-10-31 17:44:09 +0800

    ServiceNow RITM is been updated with WorkInProgress state

--- brocade_san_zone_activation.py
+++ brocade_san_zone_activation.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python
 # The term 'Broadcom' refers to Broadcom Inc. and/or its subsidiaries.
 
-from wsgiref import headers
+from urllib import request
 import requests
+import json
 
 
 documentation = '''
@@ -17,7 +18,8 @@ Functions:
     Permanently saving switch configuration
     Logout from the Brocade switch
 '''
-
+sys_id = ""
+number = ""
 
 class BrocadeZoneActivation:
 
@@ -41,8 +43,8 @@ class BrocadeZoneActivation:
         """Fetching the required values from ServiceNow RITM table to execute automation pipeline"""
         print("Fetching the required values from ServiceNow RITM table to execute automation pipeline")
         url = "https://" + self.snow_endpoint + "/api/now/table/sc_req_item/0c75a4162f3a5d107572fe7cf699b680?sysparm_fields=sys_id%2Cnumber%2Cstate%2Cvariables.ip%2Cvariables.alias%2Cvariables.wwn_1%2Cvariables.wwn_2%2Cvariables.zone"
-        user = "admin"
-        password = "e0uRn=Ph$J4Y"
+        user = self.snow_user
+        password = self.snow_pass
         headers = {"Content-Type":"application/json","Accept":"application/json"}
         response = requests.get(url, auth=(user, password), headers=headers)
         if response.status_code != 200:
@@ -50,7 +52,27 @@ class BrocadeZoneActivation:
 
         data = response.json()
         print(data)
+        global sys_id
+        sys_id = data['result']['sys_id']
+        global number
+        number = data['result']['number']
+
+
+    def serviceNow_update_record(self):
+        """Updating the RITM record to work-in-progress state within ServiceNow portal"""
+        print("Updating the RITM record to work-in-progress within ServiceNow portal")
+        url = "https://" + self.snow_endpoint + "/api/now/table/sc_req_item/" + sys_id
+        user = self.snow_user
+        password = self.snow_pass
+        headers = {"Content-Type":"application/json","Accept":"application/json"}
+        response = requests.patch(url, auth=(user, password), headers=headers, data='{\"state\":\"2\"}')
+        if response.status_code != 200:
+            print('Status:', response.status_code, 'Headers:', response.headers, 'Error Response:',response.json())
+        
+        data = response.json()
+        print("RITM state = ", data['result']['state'])
 
 
 brocade = BrocadeZoneActivation('dev78611.service-now.com', 'admin', 'e0uRn=Ph$J4Y', '10.60.22.214', 'admin', 'ctcemc123', 'Axel_spa_A1Port3_Test, Rodge_spa_A4Port3_Test', '50:06:01:63:08:60:1d:e8, 50:06:01:63:08:64:0f:45', 'Axel_Rodge_SPA_Test')
 brocade.servicenow_read_data()
+brocade.serviceNow_update_record()
