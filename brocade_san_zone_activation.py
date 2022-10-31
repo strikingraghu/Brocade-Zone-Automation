454f735021cc30a1bcd3f3ae9b1f76fd4f59ac51
strikingraghu <strikingraghu@gmail.com>
2022-10-31 13:53:55 +0800

    ServiceNow API calls are invoked

--- brocade_san_zone_activation.py
+++ brocade_san_zone_activation.py
@@ -1,6 +1,10 @@
 #!/usr/bin/python
 # The term 'Broadcom' refers to Broadcom Inc. and/or its subsidiaries.
 
+from wsgiref import headers
+import requests
+
+
 documentation = '''
 Python code developed for automating 'Brocade SAN Zoning' tasks performed by Network/Storage team.
 Functions:
@@ -14,11 +18,14 @@ Functions:
     Logout from the Brocade switch
 '''
 
+snow_endpoint = ""
+
 class BrocadeZoneActivation:
 
     """Code developed for automating 'Brocade SAN Zoning' tasks performed by Network/Storage team."""
 
     def __init__(self, snow_endpoint, snow_user, snow_pass, brocade_ip, username, password, alias_name, wwn, zone_name):
+        """Default constructor to initialize the object with values"""
         self.snow_ip = snow_endpoint
         self.snow_user = snow_user
         self.snow_pass = snow_pass
@@ -31,5 +38,23 @@ class BrocadeZoneActivation:
         print("Default constructor being invoked")
 
 
-    def service_now():
-        print("Closing the ServiceNow calls")
+    def servicenow_read_data():
+        """Fetching the required values from ServiceNow RITM table to execute automation pipeline"""
+        url = "https://" + snow_endpoint + "/api/now/table/sc_req_item/0c75a4162f3a5d107572fe7cf699b680?\
+            sysparm_fields=sys_id%2Cnumber%2Cstate%2C\
+                variables.ip%2C\
+                variables.alias%2C\
+                variables.wwn_1%2C\
+                variables.wwn_2%2C\
+                variables.zone"
+        user = "admin"
+        password = "e0uRn=Ph$J4Y"
+        headers = {"Content-Type":"application/json","Accept":"application/json"}
+        response = requests.get(url, auth=(user, password), headers=headers)
+        if response.status_code != 200:
+            print('Status:', response.status_code, 'Headers:', response.headers, 'Error Response:',response.json())
+
+        data = response.json()
+        print(data)
+
+        
