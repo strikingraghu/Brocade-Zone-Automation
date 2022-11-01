048fb5e52b49f0ae846c10f0f46d018e515f4dcc
strikingraghu <strikingraghu@gmail.com>
2022-11-01 13:27:35 +0800

    Added docstrings to the functions!

--- brocade_san_zone_activation.py
+++ brocade_san_zone_activation.py
@@ -22,12 +22,23 @@ Functions:
 sys_id = ""
 number = ""
 
-class BrocadeZoneActivation:
 
+class BrocadeZoneActivation:
     """Code developed for automating 'Brocade SAN Zoning' tasks performed by Network/Storage team."""
 
     def __init__(self, snow_endpoint, snow_user, snow_pass, brocade_ip, username, password, alias_name, wwn, zone_name):
-        """Default constructor to initialize the object with values"""
+        """
+        :param snow_endpoint: ServiceNow dev instance endpoint
+        :param snow_user: ServiceNow username for RestAPI calls
+        :param snow_pass: ServiceNow password for calling RestAPI calls
+        :param brocade_ip: Brocade dev instance IP address
+        :param username: Brocade Fabric OS RestAPI username
+        :param password: Brocade Fabric OS RestAPI password
+        :param alias_name: Alias for the Host to Storage zone
+        :param wwn: World-wide Name of HBA (Node)
+        :param zone_name: Host to Storage mapping zone name
+        """
+
         self.snow_endpoint = snow_endpoint
         self.snow_user = snow_user
         self.snow_pass = snow_pass
@@ -39,17 +50,22 @@ class BrocadeZoneActivation:
         self.zone_name = zone_name
         print("Default constructor being invoked")
 
-
     def servicenow_read_data(self):
-        """Fetching the required values from ServiceNow RITM table to execute automation pipeline"""
+        """
+        :param self: 'self' parameter is a reference to the current instance of the class
+        :return: RITM sysid required for subsequent calls
+        :return: RITM number, where this is coming from ServiceNow RITM table
+        """
         print("Fetching the required values from ServiceNow RITM table to execute automation pipeline")
-        url = "https://" + self.snow_endpoint + "/api/now/table/sc_req_item/0c75a4162f3a5d107572fe7cf699b680?sysparm_fields=sys_id%2Cnumber%2Cstate%2Cvariables.ip%2Cvariables.alias%2Cvariables.wwn_1%2Cvariables.wwn_2%2Cvariables.zone"
+        url = "https://" + self.snow_endpoint + "/api/now/table/sc_req_item/0c75a4162f3a5d107572fe7cf699b680?" \
+                                                "sysparm_fields=sys_id%2Cnumber%2Cstate%2Cvariables.ip%2C" \
+                                                "variables.alias%2Cvariables.wwn_1%2Cvariables.wwn_2%2Cvariables.zone"
         user = self.snow_user
         password = self.snow_pass
-        headers = {"Content-Type":"application/json","Accept":"application/json"}
+        headers = {"Content-Type": "application/json", "Accept": "application/json"}
         response = requests.get(url, auth=(user, password), headers=headers)
         if response.status_code != 200:
-            print('Status:', response.status_code, 'Headers:', response.headers, 'Error Response:',response.json())
+            print('Status:', response.status_code, 'Headers:', response.headers, 'Error Response:', response.json())
 
         data = response.json()
         print(data)
@@ -58,22 +74,26 @@ class BrocadeZoneActivation:
         global number
         number = data['result']['number']
 
-
-    def serviceNow_update_record(self):
-        """Updating the RITM record to work-in-progress state within ServiceNow portal"""
+    def servicenow_update_record(self):
+        """
+        :param self: 'self' parameter is a reference to the current instance of the class
+        :return: None
+        """
         print("Updating the RITM record to work-in-progress within ServiceNow portal")
         url = "https://" + self.snow_endpoint + "/api/now/table/sc_req_item/" + sys_id
         user = self.snow_user
         password = self.snow_pass
-        headers = {"Content-Type":"application/json","Accept":"application/json"}
+        headers = {"Content-Type": "application/json", "Accept": "application/json"}
         response = requests.patch(url, auth=(user, password), headers=headers, data='{\"state\":\"2\"}')
         if response.status_code != 200:
-            print('Status:', response.status_code, 'Headers:', response.headers, 'Error Response:',response.json())
-        
+            print('Status:', response.status_code, 'Headers:', response.headers, 'Error Response:', response.json())
+
         data = response.json()
         print("RITM state = ", data['result']['state'])
 
 
-brocade = BrocadeZoneActivation('dev78611.service-now.com', 'admin', 'e0uRn=Ph$J4Y', '10.60.22.214', 'admin', 'ctcemc123', 'Axel_spa_A1Port3_Test, Rodge_spa_A4Port3_Test', '50:06:01:63:08:60:1d:e8, 50:06:01:63:08:64:0f:45', 'Axel_Rodge_SPA_Test')
+brocade = BrocadeZoneActivation('dev78611.service-now.com', 'admin', 'e0uRn=Ph$J4Y', '10.60.22.214', 'admin',
+                                'ctcemc123', 'Axel_spa_A1Port3_Test, Rodge_spa_A4Port3_Test',
+                                '50:06:01:63:08:60:1d:e8, 50:06:01:63:08:64:0f:45', 'Axel_Rodge_SPA_Test')
 brocade.servicenow_read_data()
-brocade.serviceNow_update_record()
+brocade.servicenow_update_record()
